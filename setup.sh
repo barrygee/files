a3380ad79481c75d8f892d25a2ebfe9771728b20
Barry Gee <barrygee@users.noreply.github.com>
2020-08-14 20:11:09 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,35 +7,47 @@
 #                                                #
 ##################################################
 
+# Raspi-config
+host_name='RPi_HOST_NAME'
+password='PI_USER_PASSWORD'
+resolution='82' # DMT Mode 82 1920x1080 60Hz 16:9
+enable_ssh=1    # 1 = true | 0 = false
+enable_vnc=1    # 1 = true | 0 = false
+
+# Host
+rpi_ip_address='XX.X.X.X'
+latitude='XX.XXX'
+longitude='-X.XXX'
+
 # /etc/rc.local
 usb_buffer_memory='500'
 
 # .bash_aliases
 create_and_configure_bash_aliases=true
-aprs_alias='executeaprs'
-sdr_alias='executesdr'
-launch_command='launchsequence'
+aprs_alias='startaprs'
+sdr_alias='startsdr'
+adsb_alias='startadsb'
 
 # RTL_SDR
 install_rtl_sdr_tools=true
+rtl_tcp_sample_rate='2048000'
 
 # Direwolf (APRS)
 install_direwolf=true
-callsign='XXXXXX-XX'
+direwolf_conf_file=sdr.conf # sdr.conf = RX only | direwolf.conf RX and TX
+callsign='XXXXX-XX'
 callsign_pin='XXXXX'
 igserver='euro.aprs2.net'
 
-# HackRF tools
-install_hackrf_tools=false
+# Dump1090 (ADS-B)
+install_dump1090=true
+dump1090_port=9999
 
 # SoapySDR
 install_soapy_sdr=false
 
 # RX tools
 install_rx_tools=false # requires SoapySDR
-
-# Dump1090 (ADS-B)
-install_dump1090=false
 
 # RTL_433
 install_rtl_433=false
@@ -66,13 +78,26 @@ sudo apt autoremove -y
 #                                                #
 #               Update raspi-config              #
 #     Automates making changes that would be     #
-#          made by running raspi-config          #
+#       made by running sudo raspi-config        #
 #                                                #
 ##################################################
 
-#echo 'Updating /boot/config.txt'
+echo 'Updating raspi-config'
 
-#sed -i "s/MYCALL N0CALL/MYCALL $callsign/g" /boot/config.txt &&
+echo 'Setting Hostname'
+sudo raspi-config nonint do_hostname $host_name
+
+echo 'Setting new password for pi user'
+echo "pi:$password" | sudo chpasswd
+
+echo 'Setting Resolution to DMT Mode 82 1920x1080 60Hz 16:9'
+sudo raspi-config nonint do_resolution 2 $resolution
+
+echo 'Setting SSH'
+sudo raspi-config nonint do_ssh $enable_ssh
+
+echo 'Setting VNC'
+sudo raspi-config nonint do_vnc $enable_vnc
 
 
 
@@ -165,24 +190,79 @@ if $install_direwolf ; then
     make install-conf &&
     make install-rpi &&
 
-    # if /home/pi/.bash_aliases file does not exist
+    #########################################
+    #                                       #
+    #         update configuration          #
+    #                                       #
+    #########################################
+
+    # sdr.conf is used if the APRS station is receive only so does not rebroadcast APRS packets over RF
+    # does not beacon and works as an iGate
+    if [ -e ~/sdr_tools/direwolf/sdr.conf ]; then
+
+        echo 'Configuring Direwolf sdr.conf'
+
+        # update sdr.conf configuration
+        sed -i "s/MYCALL xxx/MYCALL $callsign/g" ~/sdr_tools/direwolf/sdr.conf &&
+        sed -i "s/IGSERVER noam.aprs2.net/IGSERVER $igserver/g" ~/sdr_tools/direwolf/sdr.conf &&
+        sed -i "s/IGLOGIN xxx 123456/IGLOGIN $callsign $callsign_pin/g" ~/sdr_tools/direwolf/sdr.conf &&
+
+        # requires updating only if sdr.conf | RX only is being used
+        if [ -e ~/sdr_tools/direwolf/dw-start.sh ]; then
+
+            echo 'Updating dw-start.sh'
+
+            sed -i 's/DWCMD="$DIREWOLF -a 100"/#DWCMD="$DIREWOLF -a 100"/g' ~/sdr_tools/direwolf/dw-start.sh &&
+            sed -i "s/#DWCMD=\"bash -c 'rtl_fm -f 144.39M - | direwolf -c sdr.conf -r 24000 -D 1 -'\"/DWCMD=\"bash -c 'rtl_fm -f 144.80M - | direwolf -c sdr.conf -r 24000 -D 1 -'\"" &&
+        fi
+    fi
+
+
+    # direwolf.conf is used if the APRS station both receives and transmits APRS packets over RF,
+    # beacons and works as an iGate
     if [ -e ~/sdr_tools/direwolf/direwolf.conf ]; then
 
-        ##################################################
-        #                                                #
-        #         update Direwolf configuration          #
-        #                                                #
-        ##################################################
-
-        echo 'Configuring Direwolf'
+        echo 'Configuring Direwolf direwolf.conf'
 
         # update direwolf configuration
         sed -i "s/MYCALL N0CALL/MYCALL $callsign/g" ~/sdr_tools/direwolf/direwolf.conf &&
         sed -i "s/#IGSERVER noam.aprs2.net/IGSERVER $igserver/g" ~/sdr_tools/direwolf/direwolf.conf &&
-        sed -i "s/#IGLOGIN WB2OSZ-5 123456/IGLOGIN $callsign $callsign_pin/g" ~/sdr_tools/direwolf/direwolf.conf
+        sed -i "s/#IGLOGIN WB2OSZ-5 123456/IGLOGIN $callsign $callsign_pin/g" ~/sdr_tools/direwolf/direwolf.conf &&
+        sed -i "s/#PBEACON sendto=IG delay=0:30 every=60:00 symbol=\"igate\" overlay=R lat=42^37.14N long=071^20.83W/PBEACON sendto=IG delay=0:30 every=60:00 symbol=\"igate\" overlay=R lat=$latitude long=$longitude/g" ~/sdr_tools/direwolf/direwolf.conf &&
     fi
 
+
+    echo 'Deleting unneeded Direwolf files'
+
+    cd ~/ &&
+    rm dw-start.sh &&
+    rm sdr.conf &&
+    rm telem-balloon.conf &&
+    rm telem-m0xer-3.txt &&
+    rm telem-volts.conf &&
+
 fi # end - install Direwolf (APRS)
+
+
+
+##################################################
+#                                                #
+#                Dump1090 (ADS-B)                #
+#                                                #
+##################################################
+
+if $install_dump1090 ; then
+
+    echo 'Installing Dump1090'
+
+    cd ~/sdr_tools &&
+    git clone https://github.com/antirez/dump1090.git &&
+    cd dump1090 &&
+    echo 'Updating Dump1090 Makefile' &&
+    sed -i "s/LDLIBS+=$(shell pkg-config --libs librtlsdr) -lpthread -lm/LDLIBS+=-lrtlsdr -L -lpthread -lm/g" ~/sdr_tools/dump1090/Makefile &&
+    make
+
+fi # end - install Dump1090
 
 
 
@@ -205,9 +285,23 @@ if $create_and_configure_bash_aliases ; then
         echo 'Adding aliases to .bash_aliases file'
 
         # append content to the end of the .bash_aliases file
-        echo "alias $aprs_alias='screen -d -m -S $aprs_alias sh -c \"rtl_fm -d 1 -f 144.800M | direwolf -c ~/sdr_tools/direwolf/direwolf.conf -r 24000 -D 1 -\"'" >> ~/.bash_aliases &&
-        echo "alias $sdr_alias='screen -d -m -S $sdr_alias sh -c \"rtl_tcp -d 0 -a 10.0.4.2 -s 2048000 -b 100\"'" >> ~/.bash_aliases &&
-        echo "alias $launch_command=$aprs_alias && $sdr_alias" >> ~/.bash_aliases
+        if $install_direwolf ; then
+            echo "alias $aprs_alias='screen -d -m -S $aprs_alias sh -c \"rtl_fm -d 1 -f 144.800M | direwolf -c ~/sdr_tools/direwolf/$direwolf_conf_file -r 24000 -D 1 -\"'" >> ~/.bash_aliases &&
+        fi
+
+        if $install_rtl_sdr_tools ; then
+            echo "alias $sdr_alias='screen -d -m -S $sdr_alias sh -c \"rtl_tcp -d 0 -a $rpi_ip_address -s $rtl_tcp_sample_rate -b 100\"'" >> ~/.bash_aliases &&
+
+            # TO TEST
+            # Stream audio from rtl_fm to http ogg audio
+            # rtl_fm -g50 -f 118.37M -M am -s 180k -E deemp | sox -traw -r180k -es -b16 -c1 -V1 - -t flac - | cvlc - --sout ‘#standard{access=http,mux=ogg,dst=localhost:8080/audio.ogg}’
+        fi
+
+        if $install_dump1090 ; then
+            # ref: --device flag
+            # https://github.com/antirez/dump1090/issues/129
+            echo "alias $dump1090_alias='screen ./dump1090 --enable-agc --aggressive --interactive --net --net-http-port $dump1090_port'" >> ~/.bash_aliases &&
+        fi
     fi
 
     # if the ~/.bash_aliases file exists
@@ -221,43 +315,7 @@ fi # end - .bash_aliases
 
 
 
-##################################################
-#                                                #
-#                 HACKRF Tools                   #
-#                                                #
-##################################################
-
-if $install_hackrf_tools ; then
-
-    echo 'Installing HackRF tools'
-
-    cd ~/sdr_tools &&
-    git clone https://github.com/mossmann/hackrf.git &&
-    cd hackrf/host &&
-    mkdir build && 
-    cd build &&
-    cmake ../ -DINSTALL_UDEV_RULES=ON 
-    make &&
-    sudo make install &&
-    sudo ldconfig
-
-fi # end - install HackRF tools
-
-
-
-##################################################
-#                                                #
-#                   SoapySDR                     #
-#                                                #
-##################################################
-
-if $install_soapy_sdr ; then
-    # to do
-
-    echo 'Installing SoapySDR'
-
-fi # end - install SoapySDR
-
+# ******************* TO DO  ******************* #
 
 
 ##################################################
@@ -290,21 +348,7 @@ fi # end - install RX tools
 
 ##################################################
 #                                                #
-#                Dump1090 (ADS-B)                #
-#                                                #
-##################################################
-
-if $install_dump1090 ; then
-
-    echo 'Installing Dump1090'
-
-fi # end - install Dump1090
-
-
-
-##################################################
-#                                                #
-#                     RTL433                     #
+#                 RTL433 - TO DO                 #
 #                                                #
 ##################################################
 
@@ -318,7 +362,7 @@ fi # end - install RTL433
 
 ##################################################
 #                                                #
-#                 RTL_SDR_Airband                #
+#             RTL_SDR_Airband - TO DO            #
 #                                                #
 ##################################################
 
